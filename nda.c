210cf60b3f4036f52a1680242d6495fcbae88e07
Deepak <lawaniyadeepak884@gmail.com>
2026-05-31 10:34:32 +0530

    nda.c

---
+++ nda.c
@@ -0,0 +1,11 @@
+#include < stdio.h >         
+int main() {         
+    printf("This is a Non-Disclosure Agreement (NDA) template.\n");         
+    printf("Please read and understand the terms before signing.\n");         
+    printf("1. Confidential Information: All information shared between parties is confidential.\n");         
+    printf("2. Obligations: Both parties agree to keep the information confidential and not disclose it to third parties.\n");         
+    printf("3. Term: This agreement is effective from the date of signing and will continue until terminated by either party.\n");         
+    printf("4. Governing Law: This agreement shall be governed by the laws of the jurisdiction in which it is signed.\n");         
+    printf("5. Signatures: Both parties must sign this agreement to indicate their acceptance of the terms.\n");         
+    return 0;     
+}
