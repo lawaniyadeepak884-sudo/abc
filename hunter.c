427ec3517d27e9b5b7a1a41b24c34bb8d3cb1cea
Deepak <lawaniyadeepak884@gmail.com>
2026-05-31 11:15:59 +0530

    KKL

---
+++ hunter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!");
+    printf("as a final act of love i will chase of you till i get you");//motivation
+    printf("i will never give up on you");//dedication
+    printf("i will always be there for you");//commitment
+    printf("i will always support you");//encouragement
+    printf("i will always support you");//encouragement
+printf("i will never leave you alone");//commitment
+PRINTF("i will always be there for you");//commitment
+    return 0;
+
+}
