34a442f661bddb32bf70e7377fe2180ddcaabdbb
Deepak <lawaniyadeepak884@gmail.com>
2026-05-31 10:13:14 +0530

    oanvnva

--- hello.c
+++ hello.c
@@ -5,5 +5,9 @@ int main(){
     int b= 10;
     int sum = a + b;
     printf("The sum of %d and %d is %d", a, b, sum);
+    int c=23;
+    int d=24;
+    int sum1 = c + d;
+    printf("The sum of %d and %d is %d", c, d, sum1);
     return 0;
 }
